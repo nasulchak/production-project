14b313d6f9a37f31549832d34bebcba54bd686c7
nasulchak <a.nasulchak@gmail.com>
2024-06-14 22:25:38 +0300

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,44 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin, { loader } from "mini-css-extract-plugin"
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]'
+                    },
+
+                }
+            },
+            "sass-loader",
+        ],
+    }
+
+    const assestLoader =         {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext][query]',
+        }
+    }
+
+    return [
+        typescriptLoader,
+        cssLoader,
+        assestLoader
+    ]
+}
